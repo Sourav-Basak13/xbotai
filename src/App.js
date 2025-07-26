65decd429ba3e431967147c313d56b2f34d856a6
Sourav Basak <sourav.new2016@gmail.com>
2025-07-27 00:33:11 +0530

    completed xlogin

--- src/App.js
+++ src/App.js
@@ -1,24 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+
+const intialData = {
+  username: "",
+  password: "",
+};
 
 function App() {
+  const [formState, setFormState] = useState(intialData);
+  const [error, setError] = useState(false);
+  const [isLogin, setIsLogin] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (formState.username === "user" && formState.password === "password") {
+      setIsLogin(true);
+      error && setError(false);
+      // setFormState(intialData);
+    } else {
+      setError(true);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <form onSubmit={handleSubmit}>
+      <h1>Login Page</h1>
+      {error && <p>Invalid username or password</p>}
+      {!isLogin && (
+        <>
+          <div>
+            <label>Username:</label>
+            <input
+              value={formState.username}
+              placeholder="username"
+              onChange={(e) =>
+                setFormState((prev) => ({ ...prev, username: e.target.value }))
+              }
+              required
+            />
+          </div>
+          <div>
+            <label>Password:</label>
+            <input
+              value={formState.password}
+              placeholder="password"
+              onChange={(e) =>
+                setFormState((prev) => ({ ...prev, password: e.target.value }))
+              }
+              required
+            />
+          </div>
+          <button type="submit">Submit</button>
+        </>
+      )}
+      {isLogin && <p>Welcome, {formState.username}!</p>}
+    </form>
   );
 }
 
